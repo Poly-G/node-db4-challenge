0e2344fddee0f15f8d976884d8ce5cecc1df6aea
Poly Goveia <poly.goveia@gmail.com>
2019-08-30 17:28:12 -0700

    add knex file

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/database.db3"
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    }
+  }
+};
